17899c8df3a4839b9de3facfdceafc2c3bfc3f7a
Hela Degachi <hela.degachi@esprit.tn>
2020-11-02 15:44:54 +0100

    Sonar Entreprise

--- src/test/java/emplyeeTEST.java
+++ src/test/java/emplyeeTEST.java
@@ -8,7 +8,7 @@ public class emplyeeTEST {
 	l.info("In getAllPrducts() : ");
 
 	l.debug("Je vais lancer la divsion.");
-	int i = 1/0;
+	//int i = 1/0;
 	l.debug("Je viens de lancer la divsion. " + i);
 	l.debug("Je viens de finir l'opération X.");
 	l.info("Out getAllPrducts() without errors.");
